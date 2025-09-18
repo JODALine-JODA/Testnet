143bb20b5e2fd3d8ede488984e706a1d0e7cfb1e
Dr Chima Moses Amadi <chikal0208@gmail.com>
2025-09-18 02:12:51 +0200

    Create JodaToken.sol

---
+++ Contract/JodaToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract JodaToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    uint256 public constant MAX_SUPPLY = 1_000_000_000e18; // 1B JODA
+
+    constructor(address admin) ERC20("Jodaline • JODA", "JODA") {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+    }
+
+    function mint(address to, uint256 amount) external onlyRole(MINTER_ROLE) {
+        require(totalSupply() + amount <= MAX_SUPPLY, "MAX_SUPPLY");
+        _mint(to, amount);
+    }
+}
