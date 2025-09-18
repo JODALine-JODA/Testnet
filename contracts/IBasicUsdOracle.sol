7f2bab3fa7f7db23be196bd0f84f339d1c244f4b
Dr Chima Moses Amadi <chikal0208@gmail.com>
2025-09-18 15:33:08 +0200

    Update IBasicUsdOracle.sol

--- contracts/IBasicUsdOracle.sol
+++ contracts/IBasicUsdOracle.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-/// @notice Interface for converting tokens → USD (18 decimals)
 interface IBasicUsdOracle {
     function toUsd(address quoteToken, uint256 amount) external view returns (uint256 usd18);
     function quoteTokens() external view returns (address wbnb, address usdt, address usdc);
